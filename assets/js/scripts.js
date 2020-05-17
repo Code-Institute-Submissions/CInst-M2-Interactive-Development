72f93fe6e09c2dbb4fa82601a5c42b9eedc0422e
MACmidiDev <macmidi.webdesigns@gmail.com>
2020-05-17 15:17:49 -0400

    Structure - Files| searchNavBar_renderData

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,8 @@
  /*
   * Merge DualMaps + geoMaps Click the map to set a new location for the Street View camera.
   *     todo: Clicks to any map render in street view.
+  *     todo: Clicks to any map update all maps 
+  *     todo: Clicks to any map update all maps 
   * 
   */
 
@@ -8,6 +10,61 @@
  var panorama;
  var hotelSearchMap;
  var infoWindow;
+ var countries = {
+  'au': {
+    center: {lat: -25.3, lng: 133.8},
+    zoom: 4
+  },
+  'br': {
+    center: {lat: -14.2, lng: -51.9},
+    zoom: 3
+  },
+  'ca': {
+    center: {lat: 62, lng: -110.0},
+    zoom: 3
+  },
+  'fr': {
+    center: {lat: 46.2, lng: 2.2},
+    zoom: 5
+  },
+  'de': {
+    center: {lat: 51.2, lng: 10.4},
+    zoom: 5
+  },
+  'mx': {
+    center: {lat: 23.6, lng: -102.5},
+    zoom: 4
+  },
+  'nz': {
+    center: {lat: -40.9, lng: 174.9},
+    zoom: 5
+  },
+  'it': {
+    center: {lat: 41.9, lng: 12.6},
+    zoom: 5
+  },
+  'za': {
+    center: {lat: -30.6, lng: 22.9},
+    zoom: 5
+  },
+  'es': {
+    center: {lat: 40.5, lng: -3.7},
+    zoom: 5
+  },
+  'pt': {
+    center: {lat: 39.4, lng: -8.2},
+    zoom: 6
+  },
+  'us': {
+    center: {lat: 37.1, lng: -95.7},
+    zoom: 3
+  },
+  'uk': {
+    center: {lat: 54.8, lng: -4.6},
+    zoom: 5
+  }
+};
+
 
  function initMap() {
    var berkeley = {
@@ -31,7 +88,7 @@
        lat: -34.397,
        lng: 150.644
      },
-     zoom: 6
+     zoom: 13
    });
    infoWindow = new google.maps.InfoWindow();
 
